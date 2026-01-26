644dc30ba7c3087cc00f4c6adba4f845f9b87b73
Faye <faye@fayevr.dev>
2026-01-26 21:57:47 +0100

    chore: update packages

--- next.config.ts
+++ next.config.ts
@@ -94,11 +94,6 @@ const sentryOptions: SentryBuildOptions = {
     deleteSourcemapsAfterUpload: true,
   },
 
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
@@ -107,12 +102,6 @@ const sentryOptions: SentryBuildOptions = {
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors.
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: false,
 }
 
 const sentryNextConfig = withSentryConfig(nextConfig, sentryOptions)
