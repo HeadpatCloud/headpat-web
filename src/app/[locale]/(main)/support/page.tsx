37950553cbdcc6b5c8f1badba01102979b839edb
Faye <faye@fayevr.dev>
2026-06-01 23:33:38 +0200

    fix: change github url

--- src/app/[locale]/(main)/support/page.tsx
+++ src/app/[locale]/(main)/support/page.tsx
@@ -1,90 +1,90 @@
-import Link from "next/link"
-import { getTranslations } from "gt-next/server"
+import { getTranslations } from "gt-next/server";
+import Link from "next/link";
 
 export async function generateMetadata(props: {
-  params: Promise<{ locale: string }>
+	params: Promise<{ locale: string }>;
 }) {
-  const params = await props.params
+	const params = await props.params;
 
-  const { locale } = params
+	const { locale } = params;
 
-  const meta = await getTranslations("SupportMetadata")
+	const meta = await getTranslations("SupportMetadata");
 
-  return {
-    title: meta("title"),
-    description: meta("description"),
-    alternates: {
-      canonical: `${process.env.NEXT_PUBLIC_DOMAIN}/support`,
-      languages: {
-        en: `${process.env.NEXT_PUBLIC_DOMAIN}/en/support`,
-        de: `${process.env.NEXT_PUBLIC_DOMAIN}/de/support`,
-        nl: `${process.env.NEXT_PUBLIC_DOMAIN}/nl/support`,
-      },
-    },
-    openGraph: {
-      title: meta("title"),
-      description: meta("description"),
-      siteName: process.env.NEXT_PUBLIC_WEBSITE_NAME,
-      locale: locale,
-      type: "website",
-    },
-    metadataBase: new URL(process.env.NEXT_PUBLIC_DOMAIN!),
-  }
+	return {
+		title: meta("title"),
+		description: meta("description"),
+		alternates: {
+			canonical: `${process.env.NEXT_PUBLIC_DOMAIN}/support`,
+			languages: {
+				en: `${process.env.NEXT_PUBLIC_DOMAIN}/en/support`,
+				de: `${process.env.NEXT_PUBLIC_DOMAIN}/de/support`,
+				nl: `${process.env.NEXT_PUBLIC_DOMAIN}/nl/support`,
+			},
+		},
+		openGraph: {
+			title: meta("title"),
+			description: meta("description"),
+			siteName: process.env.NEXT_PUBLIC_WEBSITE_NAME,
+			locale: locale,
+			type: "website",
+		},
+		metadataBase: new URL(process.env.NEXT_PUBLIC_DOMAIN!),
+	};
 }
 
 export default async function Page() {
-  const t = await getTranslations("SupportPage")
-  return (
-    <div className="mx-auto mb-4 mt-8 max-w-7xl p-4">
-      <div className="px-4 sm:px-0">
-        <h3 className="text-base font-semibold leading-7">{t("helpTitle")}</h3>
-        <p className="text-muted-foreground mt-1 max-w-2xl text-sm leading-6">
-          {t("helpDescription")}
-        </p>
-      </div>
-      <div className="mt-6 border-t border-gray-100">
-        <dl className="divide-y divide-gray-100">
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6">{t("discord")}</dt>
-            <dd className="mt-1 text-sm leading-6 text-black/80 sm:col-span-2 sm:mt-0 dark:text-white/80">
-              {t("discordDescription")}{" "}
-              <Link
-                className="text-link hover:text-link/80"
-                href="https://discord.gg/EaQTEKRg2A"
-              >
-                https://discord.gg/EaQTEKRg2A
-              </Link>
-              .
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6">{t("email")}</dt>
-            <dd className="mt-1 text-sm leading-6 text-black/80 sm:col-span-2 sm:mt-0 dark:text-white/80">
-              {t("emailDescription")}{" "}
-              <Link
-                className="text-link hover:text-link/80"
-                href="mailto:help@headpat.place"
-              >
-                help@headpat.place
-              </Link>
-              .
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6">{t("github")}</dt>
-            <dd className="mt-1 text-sm leading-6 text-black/80 sm:col-span-2 sm:mt-0 dark:text-white/80">
-              {t("githubDescription")}{" "}
-              <Link
-                className="text-link hover:text-link/80"
-                href="https://github.com/Headpat-Community"
-              >
-                https://github.com/Headpat-Community
-              </Link>
-              .
-            </dd>
-          </div>
-        </dl>
-      </div>
-    </div>
-  )
+	const t = await getTranslations("SupportPage");
+	return (
+		<div className="mx-auto mb-4 mt-8 max-w-7xl p-4">
+			<div className="px-4 sm:px-0">
+				<h3 className="text-base font-semibold leading-7">{t("helpTitle")}</h3>
+				<p className="text-muted-foreground mt-1 max-w-2xl text-sm leading-6">
+					{t("helpDescription")}
+				</p>
+			</div>
+			<div className="mt-6 border-t border-gray-100">
+				<dl className="divide-y divide-gray-100">
+					<div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt className="text-sm font-medium leading-6">{t("discord")}</dt>
+						<dd className="mt-1 text-sm leading-6 text-black/80 sm:col-span-2 sm:mt-0 dark:text-white/80">
+							{t("discordDescription")}{" "}
+							<Link
+								className="text-link hover:text-link/80"
+								href="https://discord.gg/EaQTEKRg2A"
+							>
+								https://discord.gg/EaQTEKRg2A
+							</Link>
+							.
+						</dd>
+					</div>
+					<div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt className="text-sm font-medium leading-6">{t("email")}</dt>
+						<dd className="mt-1 text-sm leading-6 text-black/80 sm:col-span-2 sm:mt-0 dark:text-white/80">
+							{t("emailDescription")}{" "}
+							<Link
+								className="text-link hover:text-link/80"
+								href="mailto:help@headpat.place"
+							>
+								help@headpat.place
+							</Link>
+							.
+						</dd>
+					</div>
+					<div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt className="text-sm font-medium leading-6">{t("github")}</dt>
+						<dd className="mt-1 text-sm leading-6 text-black/80 sm:col-span-2 sm:mt-0 dark:text-white/80">
+							{t("githubDescription")}{" "}
+							<Link
+								className="text-link hover:text-link/80"
+								href="https://github.com/headpatcloud"
+							>
+								https://github.com/headpatcloud
+							</Link>
+							.
+						</dd>
+					</div>
+				</dl>
+			</div>
+		</div>
+	);
 }
--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -1,167 +1,166 @@
-"use client"
+"use client";
 
+import { SiDiscord, SiGithub } from "@icons-pack/react-simple-icons";
+import { useTranslations } from "gt-next/client";
 import {
-  BadgeHelpIcon,
-  BellIcon,
-  CalendarIcon,
-  FileCheckIcon,
-  FileIcon,
-  HeartHandshakeIcon,
-  HomeIcon,
-  LayoutPanelLeftIcon,
-  MapIcon,
-  MegaphoneIcon,
-  PencilIcon,
-  RocketIcon,
-  UserSearchIcon,
-} from "lucide-react"
-import * as React from "react"
-
-import { NavSecondary } from "@/components/sidebar/nav-secondary"
-import { NavUser } from "@/components/sidebar/nav-user"
+	BadgeHelpIcon,
+	BellIcon,
+	CalendarIcon,
+	FileCheckIcon,
+	FileIcon,
+	HeartHandshakeIcon,
+	HomeIcon,
+	LayoutPanelLeftIcon,
+	MapIcon,
+	MegaphoneIcon,
+	PencilIcon,
+	RocketIcon,
+	UserSearchIcon,
+} from "lucide-react";
+import { useTheme } from "next-themes";
+import type * as React from "react";
+import { NavSecondary } from "@/components/sidebar/nav-secondary";
+import { NavUser } from "@/components/sidebar/nav-user";
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-} from "@/components/ui/sidebar"
-import { SiDiscord, SiGithub } from "@icons-pack/react-simple-icons"
-import { useTranslations } from "gt-next/client"
-import { useTheme } from "next-themes"
-import { Separator } from "../ui/separator"
-import { NavList } from "./nav-list"
-import { TeamSwitcher } from "./team-switcher"
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+} from "@/components/ui/sidebar";
+import { Separator } from "../ui/separator";
+import { NavList } from "./nav-list";
+import { TeamSwitcher } from "./team-switcher";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const pageNames = useTranslations("PageNames")
-  const { theme, systemTheme } = useTheme()
+	const pageNames = useTranslations("PageNames");
+	const { theme, systemTheme } = useTheme();
 
-  const getLogo = (name: string) => {
-    const isDarkMode =
-      theme === "dark" || (theme === "system" && systemTheme === "dark")
-    return `/logos/${name}_${isDarkMode ? "dark" : "light"}_x250.webp`
-  }
+	const getLogo = (name: string) => {
+		const isDarkMode =
+			theme === "dark" || (theme === "system" && systemTheme === "dark");
+		return `/logos/${name}_${isDarkMode ? "dark" : "light"}_x250.webp`;
+	};
 
-  const data = {
-    navMain: [
-      {
-        name: pageNames("home"),
-        url: "/",
-        icon: HomeIcon,
-      },
-      {
-        name: pageNames("gallery"),
-        url: "/gallery",
-        icon: LayoutPanelLeftIcon,
-      },
-      {
-        name: pageNames("announcements"),
-        url: "/announcements",
-        icon: MegaphoneIcon,
-      },
-      {
-        name: pageNames("notifications"),
-        url: "/notifications",
-        icon: BellIcon,
-      },
-      {
-        name: pageNames("events"),
-        url: "/events",
-        icon: CalendarIcon,
-      },
-      {
-        name: pageNames("map"),
-        url: "/map",
-        icon: MapIcon,
-      },
-      {
-        name: pageNames("users"),
-        url: "/users",
-        icon: UserSearchIcon,
-      },
-    ],
-    secondMain: [
-      {
-        name: pageNames("myprofile"),
-        url: "/profile",
-        icon: FileIcon,
-      },
-      {
-        name: pageNames("communities"),
-        url: "/community",
-        icon: HeartHandshakeIcon,
-      },
-    ],
-    navSecondary: [
-      {
-        title: "Discord",
-        url: "https://discord.gg/EaQTEKRg2A",
-        icon: SiDiscord,
-      },
-      {
-        title: "GitHub",
-        url: "https://github.com/headpat-community/",
-        icon: SiGithub,
-      },
-    ],
-    thirdMain: [
-      {
-        name: pageNames("legal"),
-        url: "/legal",
-        icon: FileCheckIcon,
-      },
-      {
-        name: "Support",
-        url: "/support",
-        icon: BadgeHelpIcon,
-      },
-      {
-        name: pageNames("changelog"),
-        url: "/changelog",
-        icon: PencilIcon,
-      },
-    ],
-    teams: [
-      {
-        name: "Place",
-        activeName: "Headpat Place",
-        logo: getLogo("place"),
-        href: "https://headpat.place",
-      },
-      {
-        name: "Space",
-        logo: getLogo("space"),
-        href: "https://headpat.space",
-      },
-      {
-        name: "Developer",
-        logo: getLogo("developer"),
-        href: "https://headpat.dev",
-      },
-    ],
-  }
+	const data = {
+		navMain: [
+			{
+				name: pageNames("home"),
+				url: "/",
+				icon: HomeIcon,
+			},
+			{
+				name: pageNames("gallery"),
+				url: "/gallery",
+				icon: LayoutPanelLeftIcon,
+			},
+			{
+				name: pageNames("announcements"),
+				url: "/announcements",
+				icon: MegaphoneIcon,
+			},
+			{
+				name: pageNames("notifications"),
+				url: "/notifications",
+				icon: BellIcon,
+			},
+			{
+				name: pageNames("events"),
+				url: "/events",
+				icon: CalendarIcon,
+			},
+			{
+				name: pageNames("map"),
+				url: "/map",
+				icon: MapIcon,
+			},
+			{
+				name: pageNames("users"),
+				url: "/users",
+				icon: UserSearchIcon,
+			},
+		],
+		secondMain: [
+			{
+				name: pageNames("myprofile"),
+				url: "/profile",
+				icon: FileIcon,
+			},
+			{
+				name: pageNames("communities"),
+				url: "/community",
+				icon: HeartHandshakeIcon,
+			},
+		],
+		navSecondary: [
+			{
+				title: "Discord",
+				url: "https://discord.gg/EaQTEKRg2A",
+				icon: SiDiscord,
+			},
+			{
+				title: "GitHub",
+				url: "https://github.com/headpatcloud/",
+				icon: SiGithub,
+			},
+		],
+		thirdMain: [
+			{
+				name: pageNames("legal"),
+				url: "/legal",
+				icon: FileCheckIcon,
+			},
+			{
+				name: "Support",
+				url: "/support",
+				icon: BadgeHelpIcon,
+			},
+			{
+				name: pageNames("changelog"),
+				url: "/changelog",
+				icon: PencilIcon,
+			},
+		],
+		teams: [
+			{
+				name: "Place",
+				activeName: "Headpat Place",
+				logo: getLogo("place"),
+				href: "https://headpat.place",
+			},
+			{
+				name: "Space",
+				logo: getLogo("space"),
+				href: "https://headpat.space",
+			},
+			{
+				name: "Developer",
+				logo: getLogo("developer"),
+				href: "https://headpat.dev",
+			},
+		],
+	};
 
-  return (
-    <Sidebar variant="inset" {...props}>
-      <SidebarHeader>
-        <TeamSwitcher teams={data.teams} />
-      </SidebarHeader>
-      <SidebarContent>
-        <NavList projects={data.navMain} />
-        <Separator />
-        <NavList projects={data.secondMain} />
-        <Separator />
-        <NavList projects={data.thirdMain} />
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
-      </SidebarContent>
-      <SidebarFooter>
-        <Separator />
-        <span className="text-muted-foreground text-center text-sm">
-          BETA 0.8.14 <RocketIcon className="inline-block size-4" />
-        </span>
-        <Separator />
-        <NavUser />
-      </SidebarFooter>
-    </Sidebar>
-  )
+	return (
+		<Sidebar variant="inset" {...props}>
+			<SidebarHeader>
+				<TeamSwitcher teams={data.teams} />
+			</SidebarHeader>
+			<SidebarContent>
+				<NavList projects={data.navMain} />
+				<Separator />
+				<NavList projects={data.secondMain} />
+				<Separator />
+				<NavList projects={data.thirdMain} />
+				<NavSecondary items={data.navSecondary} className="mt-auto" />
+			</SidebarContent>
+			<SidebarFooter>
+				<Separator />
+				<span className="text-muted-foreground text-center text-sm">
+					BETA 0.8.14 <RocketIcon className="inline-block size-4" />
+				</span>
+				<Separator />
+				<NavUser />
+			</SidebarFooter>
+		</Sidebar>
+	);
 }
